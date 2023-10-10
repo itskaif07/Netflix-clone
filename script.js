90413da006bcf2332ea17f3d85d263a0f313a66c
Kaif <129430112+itskaif07@users.noreply.github.com>
2023-10-11 01:38:17 +0530

    Uploaded

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+document.querySelector('#elem1').addEventListener('click', (e)=>{
+    document.querySelector('#bottom1').classList.toggle('hidden')
+    document.querySelector('#plus1').classList.toggle('hidden')
+    document.querySelector('#cross1').classList.toggle('hidden')
+})
+
+document.querySelector('#elem2').addEventListener('click', (e)=>{
+    document.querySelector('#bottom2').classList.toggle('hidden')
+    document.querySelector('#plus2').classList.toggle('hidden')
+    document.querySelector('#cross2').classList.toggle('hidden')
+})
+
+document.querySelector('#elem3').addEventListener('click', (e)=>{
+    document.querySelector('#bottom3').classList.toggle('hidden')
+    document.querySelector('#plus3').classList.toggle('hidden')
+    document.querySelector('#cross3').classList.toggle('hidden')
+})
+
+
+document.querySelector('#elem4').addEventListener('click', (e)=>{
+    document.querySelector('#bottom4').classList.toggle('hidden')
+    document.querySelector('#plus4').classList.toggle('hidden')
+    document.querySelector('#cross4').classList.toggle('hidden')
+})
+
+
+document.querySelector('#elem5').addEventListener('click', (e)=>{
+    document.querySelector('#bottom5').classList.toggle('hidden')
+    document.querySelector('#plus5').classList.toggle('hidden')
+    document.querySelector('#cross5').classList.toggle('hidden')
+})
+
+
+
+document.querySelector('#elem6').addEventListener('click', (e)=>{
+    document.querySelector('#bottom6').classList.toggle('hidden')
+    document.querySelector('#plus6').classList.toggle('hidden')
+    document.querySelector('#cross6').classList.toggle('hidden')
+})
+
+
+
+
+
+
+
+
+
+
+
